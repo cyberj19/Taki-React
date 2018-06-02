eaedae6754f694e9c7a732de9f2c4fc661dbf84f
Cuzi <tommer800@gmail.com>
2018-06-03 02:29:24 +0300

    Fix heap animation

--- src/components/heap.js
+++ src/components/heap.js
@@ -14,27 +14,27 @@ class Heap extends React.Component {
             {cards} = this.state;
 
         heap.length !== cards.length && this.setState({
-            cards: [...(heap.length !== cards.length ? [...cards, {...heap[heap.length - 1]}] : [...cards.slice(0, -1)])]
+            cards: [...(heap.length !== cards.length ? [...cards, {...heap[heap.length - 1], isIn: true}] : [...cards.slice(0, -1)])]
         })
     }
     componentDidUpdate(oldProps) {
         const {heap} = oldProps,
               {cards} = this.state;
 
-        heap.length !== cards.length && this.setState({
+        heap.length !== cards.length && window.setTimeout(()=> this.setState({
             cards: [...this.setCardsWithRotate(cards)]
-        })
+        }), 10);
     }
 
     setCardsWithRotate(cards) {
-        return cards.map((card, i) => card.rotate ? card : {...card, rotate: (Math.floor((Math.random() * 50 - 25) * (i / (i + 2))))})
+        return cards.map((card, i) => card.rotate ? card : {...card, rotate: (Math.floor((Math.random() * 50 - 25) * (i / (i + 2)))), isIn: false})
     }
 
     render() {
         const {cards} = this.state;
         return <div className='pack heap'>
-            {cards && cards.map(({type, color, rotate}, i ) => <div key={i + 'heapCard'}
-                                                    className="card"
+            {cards && cards.map(({type, color, rotate, isIn}, i ) => <div key={i + 'heapCard'}
+                                                    className={`card ${isIn ? 'in' : ''}`}
                                                     style={{transform: `rotate(${rotate || 0}deg)`}}
                                                     data-card-type={type}
                                                     data-color={color}/>)}
